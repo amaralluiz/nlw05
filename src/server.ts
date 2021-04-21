7ecf3e4ead25dd78913f923d337cd098e9d6ebb8
Luiz Amaral <alaluizamaral@gmail.com>
2021-04-21 11:21:55 -0300

    Finalizacao aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,13 @@
 import express from 'express';
 
+import "./database";
+import { routes } from "./routes"
+
 const app = express();
 
-/**
- * GET = Buscas
- * POST = Criação
- * PUT = Alteração
- * DELTETE = Deletar
- * PATCH = Alterar uma informação específica
- */
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05",
-    });
-});
+app.use(routes);
 
-app.post("/", (request, response) => {
-    return response.json({ message: "Usuário salvo com sucesso!" })
-})
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
