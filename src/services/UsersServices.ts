47ebcf6d4f8cfed0b0ec44871345690fa3487615
Luiz Amaral <alaluizamaral@gmail.com>
2021-04-25 19:59:49 -0300

    Finalizacao parte 3

--- src/services/UsersServices.ts
+++ src/services/UsersServices.ts
@@ -1,14 +1,21 @@
-import { getCustomRepository } from "typeorm"
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository"
 
 
 
 class UsersService {
+    private usersRepository: Repository<User>;
+
+        constructor() {
+            this.usersRepository = getCustomRepository(UsersRepository);
+        }
     async create(email: string) {
-        const usersRepository = getCustomRepository(UsersRepository);
+        
+        
         // Verificar se usuario existe
 
-        const userExists = await usersRepository.findOne({
+        const userExists = await this.usersRepository.findOne({
             email
         })
 
@@ -17,11 +24,11 @@ class UsersService {
             return userExists;
         }
 
-        const user = usersRepository.create({
+        const user = this.usersRepository.create({
             email
         });
 
-        await usersRepository.save(user);
+        await this.usersRepository.save(user);
         // Se nao existir, salvar no DB
 
         return user;
