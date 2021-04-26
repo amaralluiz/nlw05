83f7fe0046e8dbc6de6d6e2485c0952572cb5b32
Luiz Amaral <alaluizamaral@gmail.com>
2021-04-25 23:30:34 -0300

    Fim da merda da aula 4

--- src/services/UsersServices.ts
+++ src/services/UsersServices.ts
@@ -34,6 +34,14 @@ class UsersService {
         return user;
     }
 
+    async findByEmail(email: string){
+        const user = await this.usersRepository.findOne({
+            email
+        });
+
+        return user;
+    }
+
 }
 
 export { UsersService }
