a47af24b103ac58fdd9dce9db2091c691bc881a0
Luiz Amaral <alaluizamaral@gmail.com>
2021-04-27 09:04:24 -0300

    Nao faco ideia do porque nao ta funcionando

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -46,6 +46,14 @@ class ConnectionsService {
 
         return connections;
     }
+
+    async findBySocketID(socket_id: string) {
+        const connection = await this.connectionsRepository.findOne({
+            socket_id,
+        })
+
+        return connection;
+    }
 }
 
 export { ConnectionsService }
